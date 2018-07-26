85fad3da469682b552f44d596acdb0de0384f4a2
santimoy <santimoychel99@gmail.com>
2018-07-27 00:14:37 +0530

    ABC_INS file added

---
+++ ABC-INS/Normal-Pro/Demo1.java
@@ -0,0 +1,12 @@
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      byte a= 10;
+      byte b= 10;
+      byte c=(byte) (a+b);
+      System.out.println(c);
+	}
+
+}
