ddbc4cf71187eb26ea285681cb8fff1a26fcf3f9
santimoy <santimoychel99@gmail.com>
2018-07-27 03:51:57 +0530

    ABC_INS file added

---
+++ ABC-INS/src/pattren/Acat_pattren1.java
@@ -0,0 +1,24 @@
+package pattren;
+
+import java.util.*;
+
+public class Acat_pattren1 {
+	public static void main(String args[])
+	{
+int k=4;
+int count=3;
+int i,j;
+for(i=1;i<=k;i++) {
+	if(i%2==0) {
+		System.out.print(++count);}
+		for(j=1;j<=k;j++) {
+				System.out.print(i);
+		}
+		if(i%2!=0) {
+			System.out.print(++count);}
+		     System.out.println();
+	}
+         
+}
+	}
+	
